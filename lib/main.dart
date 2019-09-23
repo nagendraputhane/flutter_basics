57c393a8200112edaa3c2cb181b97f87c8f36965
nagendra <nagendraputhane@gmail.com>
2019-09-23 12:19:51 +0530

    first day

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Hello'),
-      ),
-      body: Text('This is some text. Welcome to my app'))));
+  runApp(App());
 }
