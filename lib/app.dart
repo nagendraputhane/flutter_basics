9f3f4567c322f0a1b96da16f390cd4604c39cdf3
nagendra <nagendraputhane@gmail.com>
2019-09-27 08:08:34 +0530

    location list screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'screens/location_detail/location_detail.dart';
+import 'screens/locations/locations.dart';
 
 class App extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: LocationDetail());
+    return MaterialApp(home: Locations());
   }
 }
